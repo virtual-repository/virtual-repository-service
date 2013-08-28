e38a2168b51baf9f288bf92d1faef95181aa1ce5
Fabio Fiorellato <fabio.fiorellato@fao.org>
2013-08-28 18:05:41 +0200

    Additions of a few constant pools (as interfaces)

---
+++ virtual-repository-service/src/main/java/org/virtualrepository/service/rest/ModelConstants.java
@@ -0,0 +1,22 @@
+/**
+ * (c) 2013 FAO / UN (project: virtual-repository-service)
+ */
+package org.virtualrepository.service.rest;
+
+/**
+ * Place your class / interface description here.
+ *
+ * History:
+ *
+ * ------------- --------------- -----------------------
+ * Date			 Author			 Comment
+ * ------------- --------------- -----------------------
+ * 28 Aug 2013   Fiorellato     Creation.
+ *
+ * @version 1.0
+ * @since 28 Aug 2013
+ */
+public interface ModelConstants {
+	String TABLE   = "table";
+	String SDMX_SS = "sdmx-ss";
+}
