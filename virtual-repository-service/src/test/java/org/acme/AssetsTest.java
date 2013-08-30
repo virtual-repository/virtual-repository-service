51effac91c495283599dbf6dd22371967ed9802b
fabiosimeoni <fabio.simeoni@me.com>
2013-08-30 09:59:13 +0200

    New ErrorBarrier maps java exceptions onto error response.
    - initial support for IllegalArgumentException (400) and
    IllegalStateException (409)
    - tested Bad Request with AssetResource calls with unknown asset type

--- virtual-repository-service/src/test/java/org/acme/AssetsTest.java
+++ virtual-repository-service/src/test/java/org/acme/AssetsTest.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import static com.sun.jersey.api.client.ClientResponse.Status.*;
 import static java.util.Arrays.*;
 import static javax.ws.rs.core.MediaType.*;
 import static org.acme.utils.TestUtils.*;
@@ -32,6 +33,7 @@ import org.virtualrepository.sdmx.SdmxCodelist;
 import org.virtualrepository.service.utils.CdiProducers;
 import org.virtualrepository.spi.ServiceProxy;
 
+import com.sun.jersey.api.client.ClientResponse;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.StaxDriver;
 
@@ -41,127 +43,135 @@ import flexjson.JSONDeserializer;
  * Tests the key elements of application infrastructure and testing are in place.
  * 
  * @author Fabio Simeoni
- *
+ * 
  */
 @RunWith(Arquillian.class)
 public class AssetsTest {
 
 	private static List<Asset> csvAssets = new ArrayList<Asset>();
 	private static List<Asset> sdmxAssets = new ArrayList<Asset>();
-			
+
 	@Deployment(testable = false)
 	public static WebArchive deploy() {
 		return TestUtils.war();
 	}
-	
+
 	@BeforeClass
 	@SuppressWarnings("all")
 	public static void setup() throws Exception {
-		
-		csvAssets.add(new CsvCodelist("id1","name",0));
-		csvAssets.add(new CsvCodelist("id2","name",0));
-		sdmxAssets.add(new SdmxCodelist("urn://acme.org","id2","1.0","name"));
-		
+
+		csvAssets.add(new CsvCodelist("id1", "name", 0));
+		csvAssets.add(new CsvCodelist("id2", "name", 0));
+		sdmxAssets.add(new SdmxCodelist("urn://acme.org", "id2", "1.0", "name"));
+
 		ServiceProxy proxy1 = aProxy().with(anImporterFor(CsvCodelist.type)).get();
 		ServiceProxy proxy2 = aProxy().with(anImporterFor(SdmxCodelist.type)).get();
-		
+
 		RepositoryService service1 = aService().with(proxy1).get();
-		
-		when(proxy1.browser().discover(asList(CsvCodelist.type))).thenReturn((Iterable)csvAssets);
-		
+
+		when(proxy1.browser().discover(asList(CsvCodelist.type))).thenReturn((Iterable) csvAssets);
+
 		RepositoryService service2 = aService().with(proxy2).get();
-		when(proxy2.browser().discover(asList(SdmxCodelist.type))).thenReturn((Iterable)sdmxAssets);
-		
-		CdiProducers.repository().services().add(service1,service2);
-	}
-	
-	
-	@Test
-	public void inJson(@ArquillianResource URL context) throws Exception {
-		
-		String outcome = call().resource(at(context,path)).accept(APPLICATION_JSON).get(String.class);
-		
-		JSONDeserializer<List<?>> deserializer = new JSONDeserializer<List<?>>();
-		
-		List<?> list = deserializer.deserialize(outcome);
-		
-		assertEquals(csvAssets.size()+sdmxAssets.size(),list.size());
-		
-	}
-	
-	@Test
-	public void withSelectedTypesInJson(@ArquillianResource URL context) throws Exception {
-		
-		String outcome = call().resource(at(context,path)).queryParam(typeParam,CsvCodelist.type.name()).accept(APPLICATION_JSON).get(String.class);
-		
-		JSONDeserializer<List<?>> deserializer = new JSONDeserializer<List<?>>();
-		
-		List<?> list = deserializer.deserialize(outcome);
-		
-		assertEquals(csvAssets.size(),list.size());
-		
-	}
-	
-	@Test
-	public void selectingAllTypeIsSameAsNotSelectingThemAtAll_InJson(@ArquillianResource URL context) throws Exception {
-		
-		String outcome = call().resource(at(context,path)).queryParam(typeParam,CsvCodelist.type.name())
-														  .queryParam(typeParam,SdmxCodelist.type.name())
-														  .accept(APPLICATION_JSON).get(String.class);
-		
-		JSONDeserializer<List<?>> deserializer = new JSONDeserializer<List<?>>();
-		
-		List<?> list = deserializer.deserialize(outcome);
-		
-		assertEquals(csvAssets.size()+sdmxAssets.size(),list.size());
-		
-	}
-	
-	
-	@Test
-	public void withRefreshInJson(@ArquillianResource URL context) throws Exception {
-		
-		String outcome = call().resource(at(context,path)).accept(APPLICATION_JSON).get(String.class);
-		
-		JSONDeserializer<List<?>> deserializer = new JSONDeserializer<List<?>>();
-		
-		List<?> list = deserializer.deserialize(outcome);
-		
-		int current = list.size();
-		
-		csvAssets.add(new CsvCodelist("idnew","name",0));
-		
-		outcome = call().resource(at(context,path)).accept(APPLICATION_JSON).method("POST",String.class);
-		
-		list = deserializer.deserialize(outcome);
-		
-		assertEquals(current+1,list.size());
-		
-	}
-	
-	@Test
-	public void inXml(@ArquillianResource URL context) throws Exception {
-		
-		String outcome = call().resource(at(context,path)).accept(APPLICATION_XML).get(String.class);
-		
-		Unmarshaller um = DynamicIO.newInstance().createUnmarshaller();
-		
-		//parse it as clean map
-		List<?>  list = valueOf(um.unmarshal(new StringReader(outcome)));
-		
-		assertEquals(csvAssets.size()+sdmxAssets.size(),list.size());
-	}
-	
-	@Test
-	public void inVXml(@ArquillianResource URL context) throws Exception {
-		
-		String outcome = call().resource(at(context,path)).accept(APPLICATION_VXML).get(String.class);
-		
-		List<?>  list = (List<?>) new XStream(new StaxDriver()).fromXML(outcome);
-		
-		assertEquals(csvAssets.size()+sdmxAssets.size(),list.size());
-		
+		when(proxy2.browser().discover(asList(SdmxCodelist.type))).thenReturn((Iterable) sdmxAssets);
+
+		CdiProducers.repository().services().add(service1, service2);
 	}
 
-	
+	@Test
+	public void inJson(@ArquillianResource URL context) throws Exception {
+
+		String outcome = call().resource(at(context, path)).accept(APPLICATION_JSON).get(String.class);
+
+		JSONDeserializer<List<?>> deserializer = new JSONDeserializer<List<?>>();
+
+		List<?> list = deserializer.deserialize(outcome);
+
+		assertEquals(csvAssets.size() + sdmxAssets.size(), list.size());
+
+	}
+
+	@Test
+	public void withSelectedTypesInJson(@ArquillianResource URL context) throws Exception {
+
+		String outcome = call().resource(at(context, path)).queryParam(typeParam, CsvCodelist.type.name())
+				.accept(APPLICATION_JSON).get(String.class);
+
+		JSONDeserializer<List<?>> deserializer = new JSONDeserializer<List<?>>();
+
+		List<?> list = deserializer.deserialize(outcome);
+
+		assertEquals(csvAssets.size(), list.size());
+
+	}
+
+	@Test
+	public void selectingAllTypeIsSameAsNotSelectingThemAtAll_InJson(@ArquillianResource URL context) throws Exception {
+
+		String outcome = call().resource(at(context, path)).queryParam(typeParam, CsvCodelist.type.name())
+				.queryParam(typeParam, SdmxCodelist.type.name()).accept(APPLICATION_JSON).get(String.class);
+
+		JSONDeserializer<List<?>> deserializer = new JSONDeserializer<List<?>>();
+
+		List<?> list = deserializer.deserialize(outcome);
+
+		assertEquals(csvAssets.size() + sdmxAssets.size(), list.size());
+
+	}
+
+	@Test
+	public void selectingUnknownTypesIsa400(@ArquillianResource URL context) throws Exception {
+
+		ClientResponse response = call().resource(at(context, path)).queryParam(typeParam, "bad")
+				.accept(APPLICATION_JSON).get(ClientResponse.class);
+		
+		assertEquals(BAD_REQUEST, response.getClientResponseStatus());
+		
+
+	}
+
+	@Test
+	public void withRefreshInJson(@ArquillianResource URL context) throws Exception {
+
+		String outcome = call().resource(at(context, path)).accept(APPLICATION_JSON).get(String.class);
+
+		JSONDeserializer<List<?>> deserializer = new JSONDeserializer<List<?>>();
+
+		List<?> list = deserializer.deserialize(outcome);
+
+		int current = list.size();
+
+		csvAssets.add(new CsvCodelist("idnew", "name", 0));
+
+		outcome = call().resource(at(context, path)).accept(APPLICATION_JSON).method("POST", String.class);
+
+		list = deserializer.deserialize(outcome);
+
+		assertEquals(current + 1, list.size());
+
+	}
+
+	@Test
+	public void inXml(@ArquillianResource URL context) throws Exception {
+
+		String outcome = call().resource(at(context, path)).accept(APPLICATION_XML).get(String.class);
+
+		Unmarshaller um = DynamicIO.newInstance().createUnmarshaller();
+
+		// parse it as clean map
+		List<?> list = valueOf(um.unmarshal(new StringReader(outcome)));
+
+		assertEquals(csvAssets.size() + sdmxAssets.size(), list.size());
+	}
+
+	@Test
+	public void inVXml(@ArquillianResource URL context) throws Exception {
+
+		String outcome = call().resource(at(context, path)).accept(APPLICATION_VXML).get(String.class);
+
+		List<?> list = (List<?>) new XStream(new StaxDriver()).fromXML(outcome);
+
+		assertEquals(csvAssets.size() + sdmxAssets.size(), list.size());
+
+	}
+
 }
